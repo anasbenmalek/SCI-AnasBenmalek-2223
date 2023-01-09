0eed7a98b7548807c0a33b1dd60fa6f6c52f86f4
anas benmalek <71811170+anasbenmalek@users.noreply.github.com>
2023-01-09 17:23:22 +0100

    Update finance.py

--- finance.py
+++ finance.py
@@ -1,3 +1,7 @@
+import numpy_financial as npf
+import matplotlib.pyplot as plt
+
+
 """
 
 Net Present value (NPV)
@@ -11,12 +15,12 @@ Ik ga gedurende 3 jaar investeren in een project waar ik initieel
 """
 
 # 1. variabelen declareren
+time = 4
 CF0 = -100000 
 CF1 = 50000
 CF2 = 50000
 CF3 = 50000
 interestrate = 0.05
-NPV = 0
 
 # 2. Lijst maken met de cashflows
 Cashflows = [CF0,CF1,CF2,CF3]
@@ -36,7 +40,7 @@ print(NPV(0.05,Cashflows))
 """
 
 # Manier 2
-import numpy_financial as npf
+
 npv = npf.npv(interestrate, Cashflows)
 print("De net present value is €%3.4f"%npv)
 
@@ -47,8 +51,24 @@ IRR : de discont rate die de NPV-formule oplost wanneer de NPV gelijk is aan nul
 
 """
 
-
 # IRR berekenen
 irr = round(npf.irr(Cashflows),4)
 print("de internal rate of return is %3.4f"%irr)
 
+"""
+
+Future Value (FV)
+FV : de waarde van een aanwinst naar een toekomstige datum, rekening houdend met de tijdswaarde van geld.
+
+"""
+
+fv = npf.fv(interestrate,time, -50000,100000)
+print("de future value is €%3.4f"%fv)
+
+# gebruik van Matplotlib
+y = [npf.fv(interestrate,i,-50000,100000) for i in range(4)]
+print([npf.fv(interestrate,i,-50000,100000) for i in range(4)])
+plt.plot(y)
+plt.ylabel("Investment Value")
+plt.xlabel ("Years " )
+plt.show()
